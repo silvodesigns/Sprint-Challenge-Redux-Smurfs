10e40d04fc88cfeec8477ec95f5552f40d09f4ba
Kevin-S <kevsilvo@gmail.com>
2019-08-04 19:10:59 -0400

    creating actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,13 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+import axios from 'axios';
+
+export const FETCH_SMURF_START ='FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS ='FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAIL ='FETCH_SMURF_FAIL';
+export const FETCH_SMURF_POST ='FETCH_SMURF_POST';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
